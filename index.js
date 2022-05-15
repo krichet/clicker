8480235f0a40eaa5ea20137323819b276b91e4fb
Max Vlasov <krichet@Maxs-MacBook-Pro.local>
2022-05-15 19:09:35 +0200

    working navigation from home to product page

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require('express')
 
 const path = require('path')
-const https = require('https');
+const https = require('https')
 
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
 const puppeteerProxy =  require('puppeteer-proxy')
 
-const proxyChain = require('proxy-chain');
+const proxyChain = require('proxy-chain')
   
 
-const http = require('http');
-const url = require('url');
-const cors = require('cors');
+const http = require('http')
+const url = require('url')
+const cors = require('cors')
 
 const app = express()
 
@@ -19,7 +19,7 @@ app.use(cors({
   origin: '*'
 }));
 
-const PORT = process.env.PORT || 85;
+const PORT = process.env.PORT || 85
 
 //start server 
 app.listen(PORT, () => {
@@ -30,35 +30,88 @@ app.listen(PORT, () => {
 
 app.get('/parser', function(req,res){
 
+  // let serfOptions = {
+  //   keywords: ["lexora bathroom", "faucet lexora"],
+  //   delay: 5
+  // }
+
   (async () => {
     const browser = await puppeteer.launch(
       {
-        headless: true,
+        headless: false,
         args: ['--no-sandbox']
       }
     );
     const page = await browser.newPage();
+    // page.setDefaultNavigationTimeout(0)
+    
 
     await page.setRequestInterception(true);
+    
   
     page.on('request', async (request) => {
       await puppeteerProxy.proxyRequest({
         page,
-        proxyUrl: 'http://lum-customer-c_1447ae37-zone-mobile-mobile:kcmsr6iz4bvo@zproxy.lum-superproxy.io:22225',
+        // proxyUrl: 'http://lum-customer-c_1447ae37-zone-mobile-mobile:kcmsr6iz4bvo@zproxy.lum-superproxy.io:22225',
+        // proxyUrl: 'http://go:dgtht9@108.168.148.93:7675',
+        proxyUrl: '',
         request,
       });
     });
 
     
-    
-    // await page.goto('https://krichet.com');
-    // await page.screenshot({ path: 'krichet.png' });
+    await page.goto('https://www.homedepot.com/', {waitUntil: 'domcontentloaded'})
 
-    await page.goto('https://techoverflow.net', {waitUntil: 'domcontentloaded'});
-    // Wait for 5 seconds
-    console.log(await page.content());
-    // Take screenshot
-    await browser.close();    
+    // await page.screenshot({ path: 'main.png' });
+    
+    await page.type('#headerSearch', 'lexora bathroom', {delay: 20})
+
+    await Promise.all([
+      console.log(await page.url()),
+      page.keyboard.press('Enter', {delay: 100}),
+      page.waitForNavigation(),
+    ]).catch(e => console.log(e))
+
+    //
+
+    const productsPage = await Promise.all([
+      // getNewPage(),
+      console.log(await page.url()),
+      // page.screenshot({ path: 'products.png' }),
+      page.click('.product-pod__title__product'),
+    ]).catch(e => console.log(e))
+
+    const singleProductPage = await Promise.all([
+      // getNewPage(),      
+      console.log(await page.url())
+      // page.screenshot({ path: 'single.png' }),
+    ]).catch(e => console.log(e))    
+
+    console.log(await browser.pages())
+
+    
+
+
+
+
+
+
+
+
+    // await browser.close()
+
+    function getNewPage() {
+      return new Promise((resolve) => {
+        browser.on('targetcreated', checkNewTarget);
+  
+        function checkNewTarget(target) {
+          if (target.type() === 'page') {
+            browser.off('targetcreated', checkNewTarget)
+            resolve(target.page())
+          }
+        }
+      });
+    }    
   
   })(); 
 
